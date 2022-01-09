ab6a01a6c799a8fd08bff9601797a9d153102651
B-Gowthami <beerakamgowthami@gmail.com>
2022-01-09 12:45:11 +0530

    commit changes

---
+++ src/UTC.java
@@ -0,0 +1,18 @@
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public class UTC {
+    public static void main(String[] args)
+    {
+        System.out.println("IST Time is:"+new Date());
+
+        System.out.println("UTC Time is: "+getCurrentUtcTime());
+    }
+
+    public static OffsetDateTime getCurrentUtcTime() {
+        OffsetDateTime dateTime;
+        dateTime = OffsetDateTime.now(ZoneOffset.UTC );
+        return dateTime;
+    }
+}
